f25adf6855ff8c53ef887bc20c521ade3b74867c
manaki-e <48690114s@gmail.com>
2022-01-28 23:28:48 +0900

    #7 #8 #9

--- script.js
+++ script.js
@@ -8,11 +8,24 @@
 
 
 
+/*　###########
+中央部分の動き
+############# */
+
+let center_icon = document.getElementById("center_icon");
+let center_reload = document.getElementById("center_reload")
+
+center_icon.addEventListener('click',function() {
+  let start =document.getElementById("start");
+  let stop =document.getElementById("stop");
+  start.classList.toggle("hidden")
+  stop.classList.toggle("hidden")
+});
 
 
-
-
-
+center_reload.addEventListener('click', function() {
+  console.log("リロード");
+});
 
 
 
